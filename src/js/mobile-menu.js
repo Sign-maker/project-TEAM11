ac96484131c4b4607149ea0252821a6c44248392
RuslanKorni <ruslankornijcenko4@gmail.com>
2023-08-12 13:26:52 +0200

    ++

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,3 @@
-/*var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-
-    var hamburgers = document.querySelectorAll(".hamburger");
-    if (hamburgers.length > 0) {
-      forEach(hamburgers, function(hamburger) {
-        hamburger.addEventListener("click", function() {
-          this.classList.toggle("is-active");
-        }, false);
-      });
-    }
-
-const menu = document.querySelector('.menu-body')
-const menuBtn = document.querySelector('.menu-icon')
-
-const body = document.body;
-
-if (menu && menuBtn) {
-    menuBtn.addEventListener('click', function(e) {
-        menu.classList.toggle('active')
-        menuBtn.classList.toggle('active')
-        body.classList.toggle('lock')
-    })
-}*/
 
 document.addEventListener('DOMContentLoaded', function() {
   const menu = document.querySelector('.menu-body');
