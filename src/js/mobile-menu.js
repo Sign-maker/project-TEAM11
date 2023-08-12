e54c2df6f573068349ba566102cdf2b08419a177
RuslanKorni <ruslankornijcenko4@gmail.com>
2023-08-12 12:45:57 +0200

    +++

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,4 @@
-var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+/*var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
 
     var hamburgers = document.querySelectorAll(".hamburger");
     if (hamburgers.length > 0) {
@@ -20,5 +20,20 @@ if (menu && menuBtn) {
         menuBtn.classList.toggle('active')
         body.classList.toggle('lock')
     })
-}
+}*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menu = document.querySelector('.menu-body');
+  const menuBtn = document.querySelector('.menu-icon');
+  const body = document.body;
+
+  if (menu && menuBtn) {
+      menuBtn.addEventListener('click', function() {
+          menu.classList.toggle('active'); // Toggle menu visibility
+          menuBtn.classList.toggle('active'); // Toggle menu icon state
+          body.classList.toggle('lock'); // Toggle body scrolling
+      });
+  }
+});
+
 
